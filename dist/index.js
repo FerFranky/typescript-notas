6d74da2b16af822b15a3838287c80a35911171e2
Fernando Olmos <fol9602@gmail.com>
2023-05-30 00:06:55 -0600

    Feat: Agregar Inferencia de datos

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const age = 20;
-if (age >= 18) {
-    console.log("Es mayor de edad");
-}
-else {
-    console.log("No es mayor de edad");
-}
---
+++ src/3-inferencia-de-tipos.ts
@@ -0,0 +1,8 @@
+// Si no asigno el tipo de dato TS infiere que es de tipo Any
+let myVariable
+
+// Esta es la asignacion comun para un tipo string
+let myVariableOne : string
+
+// Si inicializo la variable TS infiere que el tipo de dato equivale al tipo de dato que contiene (String para el siguiente ejemplo)
+let myVariableTwo= 'Hola mundo'
